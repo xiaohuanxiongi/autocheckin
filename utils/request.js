a8a43d6b2b27a75f69005dd3bd115ab3e9071a78
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2022-02-13 01:40:49 +0800

    fix: 签到报错原因查找

--- utils/request.js
+++ utils/request.js
@@ -31,6 +31,13 @@ const header = {
   'bili': {
     origin: 'https://live.bilibili.com',
     referer: 'https://live.bilibili.com/',
+    'sec-ch-ua':
+      '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
   }
 }
 
