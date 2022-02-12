9d5632b2f7dae2f20d3ea7168b31560e8a0a402b
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2022-02-13 01:48:26 +0800

    fix: 签到报错原因查找

--- src/biliLive.js
+++ src/biliLive.js
@@ -2,12 +2,13 @@ const service = require('../service');
 const { BILI } = require('../config');
 
 //  bilibili直播自动签到
-if (!BILI) {
-  console.log(`获取不到哔哩哔哩cookie,请检查设置`)
-} else {
-  service.biliLiveService(BILI).then((res) => {
-    console.log(`bilibili直播签到成功: `, res)
-  }).catch(err => {
-    console.log(`bili直播签到失败: `, err)
-  })
-}
+console.log(`运行到这了`)
+// if (!BILI) {
+//   console.log(`获取不到哔哩哔哩cookie,请检查设置`)
+// } else {
+//   service.biliLiveService(BILI).then((res) => {
+//     console.log(`bilibili直播签到成功: `, res)
+//   }).catch(err => {
+//     console.log(`bili直播签到失败: `, err)
+//   })
+// }
