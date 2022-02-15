129ae7ab4c2420a8c80aaef56b31b7bfa5df59d9
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2022-02-15 18:28:08 +0800

    fix: 修改签到时间

--- src/email.js
+++ src/email.js
@@ -4,7 +4,8 @@ const nodemailer = require('nodemailer');
 const dayjs = require('dayjs');
 const { EMAIL } = require('../config');
 
-const time = dayjs().format('YYYY-MM-DD')
+const minutes = 480 * 60 * 1000;  //  国际时间和北京时间相差8小时,60 * 8 = 480分钟
+const time = dayjs(new Date().getTime() + minutes).format('YYYY-MM-DD');
 
 let msg = `今天也是充满希望的一天呢!`;
 
