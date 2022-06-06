b7ac0407aecf2dca7c5f468f13a5458cfecd0ef7
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2022-06-06 09:01:46 +0800

    feat: 只有签到失败才发送邮件,正常则不发送

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const sendEmail  = require('./email');
 
 //  掘金
 function juejin() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     let text = '未签到';
     if(JUEJIN) {
       service.checkIn(JUEJIN).then(_ => {
@@ -18,18 +18,18 @@ function juejin() {
       }).catch(err => {
         text = '签到失败';
         console.log('掘金签到失败：', err);
-        resolve(`掘金签到：${text}`);
+        reject(`掘金签到：${text}`);
       })
     } else {
       text = '未设置cookie';
-      resolve(`掘金签到：${text}`);
+      reject(`掘金签到：${text}`);
     }
   })
 }
 
 //  百度贴吧
 function tieba() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     let text = '未签到';
     if(TIEBA) {
       service.getTbs(TIEBA).then(res => {
@@ -40,19 +40,19 @@ function tieba() {
         }).catch(err => {
           text = '签到失败';
           console.log(`贴吧签到失败`, err);
-          resolve(`百度贴吧签到：${text}`);
+          reject(`百度贴吧签到：${text}`);
         })
       })
     } else {
       text = '未设置cookie';
-      resolve(`百度贴吧签到：${text}`);
+      reject(`百度贴吧签到：${text}`);
     }
   })
 }
 
 //  哔哩哔哩
 function bili() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     let text = '未签到';
     if (BILI) {
       service.biliLiveService(BILI).then((res) => {
@@ -61,30 +61,21 @@ function bili() {
       }).catch(err => {
         text = '签到失败';
         console.log(`bili直播签到失败: `, err);
-        resolve(`哔哩哔哩签到：${text}`);
+        reject(`哔哩哔哩签到：${text}`);
       })
     } else {
       text = '未设置cookie';
-      resolve(`哔哩哔哩签到：${text}`);
+      reject(`哔哩哔哩签到：${text}`);
     }
   })
 }
 
 async function all() {
   Promise.all([juejin(), tieba(), bili() ]).then(res => {
-    sendEmail(true, res);
+    // sendEmail(true, res);
+  }).catch((err) => {
+    sendEmail(true, err);
   })
 }
 
 all();
-
-/**
- * 计算价格
- * @param oldTotalPic 原来的总金额
- * @param newTotalPic 现在的总金额
- * @param oldNumber 原来的总数
- * @param newNumber 现在的总数
- */
-function count(oldTotalPic, newTotalPic, oldNumber, newNumber) {
-  return ((oldTotalPic + newTotalPic) / (oldNumber + newNumber)).toFixed(4)
-}
